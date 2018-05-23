35d1486e363b63711c00f3f2abd8a967bced2736
Aleksei Kosorotov <alkos.network@gmail.com>
2018-05-23 23:09:33 +0500

    fix cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,21 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    identified_by :current_user_profile
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user_profile = find_verified_user_profile
     end
 
     private
 
-    def find_verified_user
+    def find_verified_user_profile
       session_key = Rails.application.config.session_options[:key]
       session_data = cookies.encrypted[session_key]
       warden_data = session_data["warden.user.user.key"]
       user_id = warden_data[0][0]
 
       if verified_user = User.find_by(id: user_id)
-        verified_user
+        verified_user.profile
       else
         reject_unauthorized_connection
       end
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,6 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_channel_#{current_user.id}"
+    stream_from "chat_channel_#{current_user_profile.id}"
   end
 
   def unsubscribed
