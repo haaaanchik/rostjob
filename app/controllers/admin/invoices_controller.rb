3e3bda154617100d1cf238087da2a1ba30cef3d3
Aleksei Kosorotov <alkos.network@gmail.com>
2018-09-14 16:20:23 +0500

    issue #42 when invoice state change to "paid" the money deposit to balance

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -5,10 +5,15 @@ class Admin::InvoicesController < Admin::ApplicationController
 
   def pay
     invoice.pay!
+    balance.deposit(invoice.amount, 'Пополнение баланса')
     redirect_to admin_invoices_path
   end
 
   private
+
+  def balance
+    invoice.profile.balance
+  end
 
   def invoice
     @invoice ||= Invoice.find(params[:id])
--- app/interactors/new_invoice/create.rb
+++ app/interactors/new_invoice/create.rb
@@ -7,7 +7,7 @@ module NewInvoice
       profile = context.profile
       invoice_params[:seller] = seller
       invoice_params[:buyer] = buyer(profile)
-      invoice_params[:goods] = goods
+      invoice_params[:goods] = goods(invoice_params[:amount].to_i)
       invoice = profile.invoices.create(invoice_params)
       context.invoice = invoice if invoice.persisted?
       context.fail!(invoice: invoice) unless invoice.persisted?
@@ -39,9 +39,9 @@ module NewInvoice
       }
     end
 
-    def goods
+    def goods(price)
       [
-        { title: 'Услуги по размещению заявки на поиск персонала', unit: 'шт', quantity: 1, price: 8000 }
+        { title: 'Услуги по размещению заявки на поиск персонала', unit: 'шт', quantity: 1, price: price }
       ]
     end
   end
