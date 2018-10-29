1ab25425a08274ba9c47028aaf0c056c0dadbe34
Aleksei Kosorotov <alkos.network@gmail.com>
2018-10-29 13:23:13 +0500

    fix profile type helpers

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,26 +1,10 @@
 module RolesHelper
   def customer?
-    employer? || agency?
+    profile_type == 'customer'
   end
 
-  def executor?
-    agency? || recruiter?
-  end
-
-  def employer?
-    profile_type == 'employer'
-  end
-
-  def agency?
-    profile_type == 'agency'
-  end
-
-  def recruiter?
-    profile_type == 'recruiter'
-  end
-
-  def employee?
-    profile_type == 'employee'
+  def contractor?
+    profile_type == 'customer'
   end
 
   def profile_type
