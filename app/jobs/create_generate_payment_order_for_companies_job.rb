0ca9c867d2a370a642d358edc1f6915634de9002
Aleksei Kosorotov <alkos.network@gmail.com>
2018-10-30 23:06:30 +0500

    issue #66 fix double withdraw

--- app/jobs/create_generate_payment_order_for_companies_job.rb
+++ app/jobs/create_generate_payment_order_for_companies_job.rb
@@ -4,6 +4,7 @@ class CreateGeneratePaymentOrderForCompaniesJob < ApplicationJob
   def perform
     Profile.contractors_companies.find_each(batch_size: 50) do |profile|
       next unless profile.balance.amount.positive?
+      profile.balance.withdraw(amount, 'Перечисление вознаграждения исполнителю')
       ::GeneratePaymentOrderJob.perform_later(profile, profile.balance.amount)
     end
   end
--- app/jobs/generate_payment_order_job.rb
+++ app/jobs/generate_payment_order_job.rb
@@ -2,7 +2,6 @@ class GeneratePaymentOrderJob < ApplicationJob
   queue_as :payment_orders
 
   def perform(profile, amount)
-    result = Cmd::PaymentOrder::Create.call(profile: profile, amount: amount)
-    profile.balance.withdraw(amount, 'Перечисление вознаграждения исполнителю') if result.success?
+    Cmd::PaymentOrder::Create.call(profile: profile, amount: amount)
   end
 end
