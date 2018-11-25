12823ca90ec4e28be9640b384c26a33c282e4b94
Артем Кушнарев <it-a@apismedia.pro>
2018-11-25 17:35:50 +0300

    new mdb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,7 +3,8 @@ require 'resque/server'
 require 'resque/scheduler/server'
 require 'active_scheduler'
 
-redis = Redis.new(host: "localhost", port: 6379)
+host = Gem.win_platform? ? '127.0.0.1' : 'localhost'
+redis = Redis.new(host: host, port: 6379)
 Resque.redis = redis
 
 yaml_schedule    = YAML.load_file("#{Rails.root}/config/schedule.yml") || {}
