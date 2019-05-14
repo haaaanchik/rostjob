5217dc89649781f2fae6430c13e814a24409069e
Aleksei Kosorotov <alkos.network@gmail.com>
2019-05-14 16:22:59 +0500

    change redis config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,6 +6,7 @@ require 'active_scheduler'
 host = Gem.win_platform? ? '127.0.0.1' : 'localhost'
 redis = Redis.new(host: host, port: 6379)
 Resque.redis = redis
+Resque.redis.namespace = "resque:#{Rails.env}"
 
 yaml_schedule    = YAML.load_file("#{Rails.root}/config/schedule.yml") || {}
 wrapped_schedule = ActiveScheduler::ResqueWrapper.wrap yaml_schedule
