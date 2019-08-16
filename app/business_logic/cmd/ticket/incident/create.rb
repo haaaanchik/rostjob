d89cbc544536c6223b3e1bf039b3a1d036b7690b
Aleksei Kosorotov <alkos.network@gmail.com>
2019-08-16 13:13:51 +0500

    add disput messages to user_action_log

--- app/business_logic/cmd/ticket/incident/create.rb
+++ app/business_logic/cmd/ticket/incident/create.rb
@@ -8,6 +8,7 @@ module Cmd
           incident = ::Incident.new(incident_params)
           context.incident = incident
           context.fail! unless context.incident.save
+          Cmd::UserActionLogger::Log.call(params: logger_params)
         end
 
         private
@@ -29,6 +30,24 @@ module Cmd
 
         def user
           context.user
+        end
+
+        def employee_cv
+          @employee_cv ||= ::ProposalEmployee.find(incident_params[:proposal_employee_id]).employee_cv
+        end
+
+        def logger_params
+          {
+            login: user.email,
+            receiver_ids: [user.id],
+            subject_id: user.id,
+            subject_type: 'User',
+            subject_role: user.profile.profile_type,
+            action: "Открыт спор по анкете №#{employee_cv.id} #{employee_cv.name}",
+            object_id: employee_cv.id,
+            object_type: 'EmployeeCv',
+            employee_cv_id: employee_cv.id
+          }
         end
       end
     end
