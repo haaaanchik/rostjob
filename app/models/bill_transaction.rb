21f5c79b10160502a5fe5340838e27d80cdad80b
Магомед Сайпудинов <saipudinov.magomed@gmail.com>
2019-10-14 11:15:38 +0300

    Fixed publish order from template and create appeal

--- app/models/bill_transaction.rb
+++ app/models/bill_transaction.rb
@@ -1,6 +1,6 @@
 class BillTransaction < ApplicationRecord
   belongs_to :balance
-  belongs_to :invoice
+  belongs_to :invoice, required: false
 
   validates :amount, presence: true, numericality: { greater_than: 0}
   validates :transaction_type, presence: true, inclusion: { in: %w[deposit withdrawal] }
