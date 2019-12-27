723f52cd83094923135cbf3b90f172fa2fb975ba
Магомед Сайпудинов <saipudinov.magomed@gmail.com>
2019-12-27 03:49:29 +0300

    FIxed jobny-ru.conf and wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://89.108.99.141/cable'
   # config.action_cable.url = 'wss://jobny.ru/cable'
-  config.action_cable.allowed_request_origins = ['http://jobny.ru']
+  config.action_cable.allowed_request_origins = ['https://jobny.ru']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
