af2d152a97029eb0de21d0a75598dfd3a9cff412
Pozetiv <rbreslavets@gmail.com>
2020-04-23 10:20:50 +0300

    add signature to offer and export him

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,8 +1,11 @@
 class TermsController < ApplicationController
-
   def index
     @terms = Terms.new
     flash[:alert] = 'Вы должны принять условия оферты, прежде чем продолжить' unless current_user.terms_of_service
+    respond_to do |format|
+    format.html
+    format.pdf { render pdf_setting }
+end
   end
 
   def accept
@@ -21,5 +24,16 @@ class TermsController < ApplicationController
 
   def terms_params
     params.require(:terms).permit(:accepted)
+  end
+
+  def pdf_setting
+    {
+      pdf: 'договор оферты RostJob',
+      template: 'terms/_term_pdf.html',
+      orientation: 'Portrait',
+      page_size: 'A4',
+      dpi: 300,
+      encoding: 'utf-8'
+    }
   end
 end
