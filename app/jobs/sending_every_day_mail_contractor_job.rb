59a33f416cce4b3e7bd9672cbe9207874d3e02ca
Pozetiv <rbreslavets@gmail.com>
2020-07-11 21:30:39 +0300

    fix mistake change name rectuter to customers name in informated_customer_has_disputed mail

--- app/jobs/sending_every_day_mail_contractor_job.rb
+++ app/jobs/sending_every_day_mail_contractor_job.rb
@@ -9,14 +9,14 @@ class SendingEveryDayMailContractorJob < ApplicationJob
   private
 
   def tomorrow_interview_contractor
-    ProposalEmployee.interview.include(profile: :setting_objects).where(interview_date: (DateTime.now.beginning_of_day + 1.day))
+    ProposalEmployee.interview.includes(profile: :setting_objects).where(interview_date: (DateTime.now.beginning_of_day + 1.day))
       .group_by { |pr| pr.profile }.each do |profile, prop_emp|
         ProposalEmployeeMailJob.perform_now(proposal_employees: prop_emp, method: 'tommorow_interview_contractor') if profile.every_day_mailing?
       end
   end
 
   def has_disputed_contractor
-    Incident.opened.includes(:proposal_employee, profile: :setting_objects).group_by { |incident| incident.proposal_employee.profile }.each do |profile, incident|
+    Incident.opened.includes(:proposal_employee, user: { profile: :setting_objects }).group_by { |incident| incident.proposal_employee.profile }.each do |profile, incident|
       SendNotifyMailJob.perform_now(objects: incident, method: 'informated_contractor_has_disputed') if profile.every_day_mailing?
     end
   end
