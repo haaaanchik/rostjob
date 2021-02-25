fd823360bec55e9a8556e7ec9fad1c93e23de4db
Khangerey Saypudinov <saypudinov05@gmail.com>
2021-02-25 07:24:39 +0300

    Adding a field to the specialization table for a picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_091852) do
+ActiveRecord::Schema.define(version: 2021_02_16_030047) do
 
   create_table "account_statements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "src_account"
@@ -551,6 +551,10 @@ ActiveRecord::Schema.define(version: 2021_01_26_091852) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["title"], name: "index_specializations_on_title"
   end
 
