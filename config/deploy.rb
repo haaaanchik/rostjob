abc6713ad7baf566fe1b264bf8e0699dac663e4b
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2021-03-18 16:38:02 +0300

    Linked public/sitemap.xml for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,6 @@ set :application, 'rostjob'
 set :repo_url, 'git@bitbucket.org:Factory-Rushers/rostjob.git'
 
 append :linked_files, 'config/master.key', 'config/database.yml'
-append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/sitemap.xml'
 
 set :keep_releases, 3
