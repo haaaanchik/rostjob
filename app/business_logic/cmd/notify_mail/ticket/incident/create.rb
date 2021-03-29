9152852c07407002b4621027b1cb7a6a4b4609b8
Pozetiv <rbreslavets@gmail.com>
2021-03-29 12:53:06 +0300

    work with incidents

--- app/business_logic/cmd/notify_mail/ticket/incident/create.rb
+++ app/business_logic/cmd/notify_mail/ticket/incident/create.rb
@@ -7,10 +7,25 @@ module Cmd
         class Create
           include Interactor
 
+          delegate :user,     to: :context
           delegate :incident, to: :context
 
           def call
-            SendNotifyMailJob.perform_now(objects: [incident], method: 'informated_contractor_has_disputed')
+            return unless send_to.profile.notify_mails?
+
+            ProposalEmployeeMailJob.perform_now(user: send_to,
+                                                proposal_employees: [incident.proposal_employee],
+                                                method: 'informated_user_has_disputed')
+          end
+
+          private
+
+          def send_to
+            user.customer? ? pr_empl.user : pr_empl.order.user
+          end
+
+          def pr_empl
+            incident.proposal_employee
           end
         end
       end
--- app/jobs/proposal_employee_mail_job.rb
+++ app/jobs/proposal_employee_mail_job.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ProposalEmployeeMailJob < ApplicationJob
   queue_as :mail
 
   def perform(args)
-    NotifyMailer.with(proposal_employees: args[:proposal_employees]).public_send(args[:method]).deliver_now
+    NotifyMailer.with(proposal_employees: args[:proposal_employees], user: args[:user])
+                .public_send(args[:method])
+                .deliver_now
   end
 end
