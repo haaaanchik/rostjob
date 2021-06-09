75d64aa3caa5ac398b0e7e4fb0b45f664ddf011e
Damir Chechuhov <chechuhov.damir@mail.ru>
2021-06-09 23:19:32 +0300

    Fix profile update error from NewRelic

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,14 +11,14 @@ class ProfilesController < ApplicationController
   def update
     @result = Cmd::Profile::Update.call(profile: current_profile, params: profile_params)
     @result.profile
-    if @result.success?
+    if @result.success
       current_profile.update_attribute(:updated_by_self_at, DateTime.now)
       redirect_to edit_profile_path, notice: 'Анкета обновлена'
     else
       render json: { validate: true, data: errors_data(@result.profile) }, status: 422
-      redirect_to edit_profile_path, alert: 'Анкета не обновлена'
     end
   end
+
 
   def set_free
     @status = if profile.phone.present?
