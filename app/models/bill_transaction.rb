5d405771a9d2dd3fdcd270e823dd278fd8995048
Khangerey Saypudinov <saypudinov05@gmail.com>
2021-06-30 15:30:58 +0300

    Changed validation for bill_transactions greater_than to greater_than_or_equal_to

--- app/models/bill_transaction.rb
+++ app/models/bill_transaction.rb
@@ -2,7 +2,7 @@ class BillTransaction < ApplicationRecord
   belongs_to :balance
   belongs_to :invoice, required: false
 
-  validates :amount, presence: true, numericality: { greater_than: 0}
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :transaction_type, presence: true, inclusion: { in: %w[deposit withdrawal] }
   validates :description, presence: true
 end
