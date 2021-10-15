7b6b53a0f0c6c2bcc5944a737c44b10ceaa065c6
Damir Chechuhov <chechuhov.damir@mail.ru>
2021-10-15 13:55:27 +0300

    I send sorted orders by user location

--- app/controllers/api/helpers/orders_helpers.rb
+++ app/controllers/api/helpers/orders_helpers.rb
@@ -3,6 +3,43 @@
 module Api
   module Helpers
     module OrdersHelpers
+
+      EARTH_RADIUS = 6378137
+
+      def calculate_closer_cities(lat, long)
+        lat0 = Math.cos(Math::PI / 180.0 * lat)
+        {
+            y0: lat - (180/Math::PI)*(distance/EARTH_RADIUS),
+            y1: lat + (180/Math::PI)*(distance/EARTH_RADIUS),
+            x0: long - (180/Math::PI)*(distance/EARTH_RADIUS)/Math.cos(lat0),
+            x1: long + (180/Math::PI)*(distance/EARTH_RADIUS)/Math.cos(lat0)
+        }
+      end
+
+      def distance
+        case params[:radius_of_cities]
+        when 'near'
+          100000.0
+        when 'next'
+          300000.0
+        when 'far'
+          5000000.0
+        else
+          1000.0
+        end
+      end
+
+      def search_orders_by_radius
+        return published_orders if params[:radius_of_cities].blank?
+
+        city = Geo::City.find_by(name: params[:q]['city_name_eq'] || params[:user_location])
+        return published_orders if city.blank?
+
+        coordinates = calculate_closer_cities(city.lat, city.long)
+        closer_city_ids = Geo::City.where( lat: coordinates[:y0]..coordinates[:y1], long: coordinates[:x0]..coordinates[:x1] ).pluck(:id)
+        Order.where(city_id: closer_city_ids).published
+      end
+
       def published_orders
         Order.published.includes(:city, :position, profile: :company)
       end
--- app/controllers/api/v1/orders.rb
+++ app/controllers/api/v1/orders.rb
@@ -9,10 +9,13 @@ module Api
       end
       params do
         optional :q, type: Hash do
+          optional :city_name_eq, type: String, desc: 'Order city.'
           optional :category_titles_in, type: Array, desc: 'Order by specialization.'
           optional :title_or_company_fields_cont, type: String, desc: 'Search by order title or company name.'
           optional :shift_method_eq, type: Boolean, desc: 'Order shift method'
         end
+        optional :user_location, type: String, desc: 'User location'
+        optional :radius_of_cities, type: String, values: %w[near next far], desc: 'Radius cities'
         optional :page, type: Integer, default: 1, desc: 'Specify the page of paginated results.'
         optional :without_experience, type: Integer, values: [0, 1], default: 0, desc: 'Orders with a price of 8000 or less'
       end
@@ -21,14 +24,21 @@ module Api
         params[:q] = {} if params[:q].blank?
         params[:q][:without_experience_field_lteq] = 8000 if params[:without_experience].positive?
 
-        result = published_orders.ransack(params[:q]).result
+        p_orders = search_orders_by_radius
+        result = p_orders.ransack(params[:q]).result
         orders = result.page(params[:page])
 
         present :total_orders, result.count
         present :total_pages, orders.total_pages
         present :orders, orders, with: Entities::Order, base_url: request.base_url
         present :categories, ActiveSpecializationsSpecification.to_scope.map(&:title)
-        present :cities, published_orders.map { |order| order.city&.name }.uniq
+        present :cities, published_orders.map { |order|
+          {
+              name: order.city&.name,
+              lat: order.city&.lat,
+              long: order.city&.long
+          }
+        }.uniq.compact
       end
 
 
