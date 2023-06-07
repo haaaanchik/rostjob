300784afd3bae61c44b83aa22b398a68593c101b
NO_USER_SPECIFIED <NO_EMAIL_SPECIFIED>
2023-06-07 19:31:03 +0600

    fix deploy scripts to not overwrite but link credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '~> 3.11.0'
 set :application, 'rostjob'
 set :repo_url, 'git@bitbucket.org:Factory-Rushers/rostjob.git'
 
-append :linked_files, 'config/master.key', 'config/database.yml'
+append :linked_files, 'config/master.key', 'config/database.yml', 'credentials.yml.enc'
 append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :keep_releases, 3
